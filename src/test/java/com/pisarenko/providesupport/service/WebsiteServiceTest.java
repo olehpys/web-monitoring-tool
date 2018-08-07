8dfb4fbd6bf5023affb4ce9362bb60e65b811d0c
Oleg <oa.pisarenko@gmail.com>
2018-08-07 23:17:45 +0300

    service unit test

---
+++ src/test/java/com/pisarenko/providesupport/service/WebsiteServiceTest.java
@@ -0,0 +1,49 @@
+package com.pisarenko.providesupport.service;
+
+import com.pisarenko.providesupport.model.ResponseTime;
+import com.pisarenko.providesupport.model.Website;
+import com.pisarenko.providesupport.repository.WebsiteRepository;
+import com.pisarenko.providesupport.service.impl.WebsiteServiceImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+public class WebsiteServiceTest {
+
+    @Mock
+    private WebsiteRepository websiteRepository;
+
+    @InjectMocks
+    private WebsiteServiceImpl websiteService;
+
+    private Website website;
+
+    @Before
+    public void setUp() {
+        website = Website.builder()
+                .id("1").url("https://telegram.org/").responseTime(ResponseTime.OK).responseCode("200")
+                .minResponseValue((byte) 1).maxResponseValue((byte) 125)
+                .build();
+    }
+
+    @Test
+    public void getAllWebsitesTest() {
+
+        Mockito.when(websiteRepository.findAll()).thenReturn(Collections.singletonList(website));
+
+        List<Website> websiteList = websiteService.getAllWebsites();
+
+        Assert.assertEquals(Collections.singletonList(website).get(0), websiteList.get(0));
+
+
+    }
+}
