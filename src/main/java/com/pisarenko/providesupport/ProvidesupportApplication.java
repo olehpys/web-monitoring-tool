35de0652ccedbd1872f1954d5a3d2f0d06bcd22e
Oleg <oa.pisarenko@gmail.com>
2018-08-07 10:37:21 +0300

    Initial commit

---
+++ src/main/java/com/pisarenko/providesupport/ProvidesupportApplication.java
@@ -0,0 +1,12 @@
+package com.pisarenko.providesupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvidesupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvidesupportApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pisarenko/providesupport/ProvidesupportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pisarenko.providesupport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProvidesupportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
