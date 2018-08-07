a82e46ac9c77e0763289ac2c17bb043b7d00a114
Oleg <oa.pisarenko@gmail.com>
2018-08-07 23:18:38 +0300

    db config and test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into websites (website_id, url, response_time, response_code, min_res_value, max_res_value)
+values ('1', 'http://vk.com/', 10, '403', 0, 3);
+
+insert into websites (website_id, url, response_time, response_code, min_res_value, max_res_value)
+values ('2', 'http://fb.com/', 3, '200', 0, 5);
