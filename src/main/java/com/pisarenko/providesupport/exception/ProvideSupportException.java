d8f6e7d7ef86317359e6bad211684ea8c9b13b0b
Oleg <oa.pisarenko@gmail.com>
2018-08-09 00:38:04 +0300

    refactoring

---
+++ src/main/java/com/pisarenko/providesupport/exception/ProvideSupportException.java
@@ -0,0 +1,25 @@
+package com.pisarenko.providesupport.exception;
+
+public class ProvideSupportException extends RuntimeException {
+
+    public ProvideSupportException() {
+        super();
+    }
+
+    public ProvideSupportException(String message) {
+        super(message);
+    }
+
+    public ProvideSupportException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+
+    public ProvideSupportException(Throwable throwable) {
+        super(throwable);
+    }
+
+    public ProvideSupportException(String message, Throwable throwable, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, throwable, enableSuppression, writableStackTrace);
+    }
+}
