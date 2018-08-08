758713ed638426356213379b456a21e224ec65ab
Oleg <oa.pisarenko@gmail.com>
2018-08-08 14:48:53 +0300

    entity for checked websites

---
+++ src/main/java/com/pisarenko/providesupport/model/WebsiteState.java
@@ -0,0 +1,37 @@
+package com.pisarenko.providesupport.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "websites_states")
+public class WebsiteState {
+
+    @Id
+    @Column(name = "website_id")
+    private Website website;
+
+    @Column(name = "website_state")
+    private StateStatus state;
+
+    @Column(name = "content_length")
+    private long contentLength;
+
+    @Column(name = "response_code")
+    private String responseCode;
+
+    @Column(name = "response_time")
+    private String responseTime;
+
+}
