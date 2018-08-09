fa77c9be10d05e9a1fdfb93e3c3bca7da1fff8d0
Oleg <oa.pisarenko@gmail.com>
2018-08-10 00:17:35 +0300

    refactoring

--- src/main/java/com/pisarenko/providesupport/controller/WebsiteController.java
+++ src/main/java/com/pisarenko/providesupport/controller/WebsiteController.java
@@ -7,46 +7,67 @@ import com.pisarenko.providesupport.service.WebsiteStateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/websites")
 public class WebsiteController {
 
     private final WebsiteService websiteService;
     private final WebsiteStateService websiteStateService;
 
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    ResponseEntity<Website> addWebsite(@RequestBody Website website) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    ModelAndView websiteStates() {
+        List<WebsiteState> websitesStates = websiteStateService.getStates(websiteService.getAllWebsites());
+        ModelAndView states = new ModelAndView("index");
+        states.addObject("websitesStates", websitesStates);
+        return states;
+    }
+
+    @RequestMapping(value = "/website/add", method = RequestMethod.POST)
+    ResponseEntity<Website> addWebsite(Website website) {
         Website createdWebsite = websiteService.createWebsite(website);
         return ResponseEntity.ok(createdWebsite);
     }
 
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
-    ResponseEntity<Void> editWebsiteConfig(@PathVariable String id) {
-        websiteService.updateWebsite(id);
+    @RequestMapping(value = "/website/edit", method = RequestMethod.POST)
+    ResponseEntity<Void> editWebsiteConfig(Website website) {
+        websiteService.updateWebsite(website);
         return ResponseEntity.ok().build();
     }
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-    ResponseEntity<Void> deleteWebsite(@PathVariable String id) {
+    @RequestMapping(value = "/website/delete", method = RequestMethod.POST)
+    ResponseEntity<Void> deleteWebsite(String id) {
         websiteService.deleteWebsite(id);
         return ResponseEntity.ok().build();
     }
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    ResponseEntity<List<Website>> websites() {
-        List<Website> websites = websiteService.getAllWebsites();
-        return ResponseEntity.ok(websites);
+    @RequestMapping(value = "/website/edit/{id}", method = RequestMethod.GET)
+    ModelAndView editWebsite(@PathVariable String id) {
+        Website websiteToUpdate = websiteService.getWebsiteById(id);
+        ModelAndView website = new ModelAndView("edit");
+        website.addObject("website", websiteToUpdate);
+        return website;
     }
 
-    @RequestMapping(value = "/allChecked", method = RequestMethod.GET)
-    ResponseEntity<List<WebsiteState>> websiteStates() {
-        List<WebsiteState> websitesStates = websiteStateService.getStates(websiteService.getAllWebsites());
-        return ResponseEntity.ok(websitesStates);
+    @RequestMapping(value = "/website/{id}", method = RequestMethod.GET)
+    ModelAndView getWebsiteState(@PathVariable String id) {
+        Website currentWebsite = websiteService.getWebsiteById(id);
+        WebsiteState currentWebsiteState = websiteStateService.getStateById(id);
+        Map<String, Object> model = new HashMap<>();
+        model.put("expectedWebsite", currentWebsite);
+        model.put("websiteState", currentWebsiteState);
+
+        return new ModelAndView("website", "model", model);
     }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into websites (website_id, url, response_time, response_code, min_res_value, max_res_value)
-values ('1', 'http://vk.com/', 10, '403', 0, 3);
+values ('1', 'https://telegram.org/', 'OK', '302', 0, 100000);
 
 insert into websites (website_id, url, response_time, response_code, min_res_value, max_res_value)
-values ('2', 'http://fb.com/', 3, '200', 0, 5);
+values ('2', 'https://fb.com/', 'OK', '200', 0, 5);
