2ed03abc91b97e46e4422c3156ecd66dd6e498d1
Oleg <oa.pisarenko@gmail.com>
2018-08-10 11:39:08 +0300

    refactoring

--- src/main/java/com/pisarenko/providesupport/model/StateStatus.java
+++ src/main/java/com/pisarenko/providesupport/model/StateStatus.java
@@ -1,16 +1,6 @@
 package com.pisarenko.providesupport.model;
 
-import lombok.Getter;
 
-@Getter
 public enum StateStatus {
-    OK(0, 5), WARNING(5, 10), CRITICAL(10, 15);
-
-    private int minTime;
-    private int maxTime;
-
-    StateStatus(int minTime, int maxTime) {
-        this.minTime = minTime;
-        this.maxTime = maxTime;
-    }
+    OK, WARNING, CRITICAL
 }
