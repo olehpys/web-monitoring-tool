b260b749e9ff8028e3c1b223a91ecb624d36b878
Oleg <oa.pisarenko@gmail.com>
2018-08-11 17:37:09 +0300

    updated test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,20 @@
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('1', 'https://telegram.org/', 1, '200', 'ACTIVE', 0, 100000, '2018-08-09', '2018-08-12');
+values ('1', 'https://telegram.org/', 1, '200', 'ACTIVE', 0, 100000, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('2', 'https://fb.com/', 5, '200', 'INACTIVE', 0, 3215, '2018-08-09', '2018-08-12');
+values ('2', 'https://fb.com/', 5, '200', 'INACTIVE', 0, 3215, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('3', 'https://kotlinlang.org/docs/reference/coroutines.html', 0.5, '200', 'ACTIVE', 0, 512, '2018-08-09', '2018-08-12');
+values ('3', 'https://kotlinlang.org/docs/reference/coroutines.html', 0.5, '200', 'ACTIVE', 0, 512, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('4', 'https://twitter.com/', 1.2, '200', 'ACTIVE', 0, 100000, '2018-08-09', '2018-08-12');
+values ('4', 'https://twitter.com/', 1.2, '200', 'ACTIVE', 0, 100000, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('5', 'https://habrahabr.ru/post/193242/', 3.3, '200', 'ACTIVE', 0, 359, '2018-08-09', '2018-08-12');
+values ('5', 'https://habrahabr.ru/post/193242/', 3.3, '200', 'ACTIVE', 0, 359, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('6', 'https://spotify.com/', 1, '200', 'INACTIVE', 0, 298, '2018-08-09', '2018-08-12');
+values ('6', 'https://spotify.com/', 1, '200', 'INACTIVE', 0, 298, '2018-08-09', '2018-08-20');
 
 insert into websites (website_id, url, response_time, response_code, website_status, min_res_value, max_res_value, date_from, date_to)
-values ('7', 'http://www.kipshop.ru/CoDeSys/steps/codesys_v23_ru.pdf', 6, '200', 'ACTIVE', 0, 5000000, '2018-08-09', '2018-08-12');
+values ('7', 'http://www.kipshop.ru/CoDeSys/steps/codesys_v23_ru.pdf', 0.2, '200', 'ACTIVE', 0, 5000000, '2018-08-09', '2018-08-20');
